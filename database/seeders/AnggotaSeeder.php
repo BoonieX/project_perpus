e6ca4e72d2d03f1877edafc3efba98351e58be61
Taufiq2205 <158468693+Taufiq2205@users.noreply.github.com>
2025-02-24 12:56:57 +0700

    fiqo

---
+++ database/seeders/AnggotaSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Anggota;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AnggotaSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Anggota::create([
+            'nama'=> 'Fiqo',
+            'alamat'=> 'Klabang',
+            'no_telp'=> '082334678958',
+            'email'=> 'fiqo@gmail.com',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 use App\Http\Controllers\BukuController;
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
 Route::resource('buku', BukuController::class);
-=======
 Route::get('/', function () {
     return view('home');
 });
->>>>>>> 424cb027baa49cf33d895e01764282b7a8a9571c
